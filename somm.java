c8083b3217ad12619cae1865b895dd7e11205050
eya-bouzidi <ayaa98114@gmail.com>
2025-02-01 18:35:23 +0100

    Create somm.java
    somm

---
+++ somm.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class somm {
+    public static void main(String args[]) {
+        System.out.print("donner A= ");
+        Scanner e = new Scanner(System.in);
+        int a = e.nextInt();
+        System.out.print("donner B= ");
+        int b = e.nextInt();
+        int c=a+b;
+        System.out.println("A+B= " + c);
+    }
+}
